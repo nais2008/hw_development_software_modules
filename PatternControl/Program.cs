2ed71132d01f03036b2b33e6fbbbdfa851df29e0
nais2008 <t09n09a08@gmail.com>
2026-06-21 03:07:49 +0500

    qwe

---
+++ PatternControl/Program.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PatternControl
+{
+    interface ICommand
+    {
+        void Execute();
+        void Undo();
+        string GetName();
+    }
+
+    class Light
+    {
+        public bool IsOn { get; private set; }
+
+        public void On()
+        {
+            IsOn = true;
+            Console.WriteLine("Свет включен");
+        }
+
+        public void Off()
+        {
+            IsOn = false;
+            Console.WriteLine("Свет выключен");
+        }
+    }
+
+    class AirConditioner
+    {
+        public bool IsOn { get; private set; }
+        public int Temperature { get; private set; } = 22;
+
+        public void On()
+        {
+            IsOn = true;
+            Console.WriteLine($"Кондиционер включен, температура {Temperature}C");
+        }
+
+        public void Off()
+        {
+            IsOn = false;
+            Console.WriteLine("Кондиционер выключен");
+        }
+
+        public void SetTemperature(int temp)
+        {
+            Temperature = temp;
+            Console.WriteLine($"Температура установлена на {Temperature}C");
+        }
+    }
+
+    class MusicSystem
+    {
+        public bool IsOn { get; private set; }
+        public int Volume { get; private set; } = 50;
+
+        public void On()
+        {
+            IsOn = true;
+            Console.WriteLine($"Музыка включена, громкость {Volume}");
+        }
+
+        public void Off()
+        {
+            IsOn = false;
+            Console.WriteLine("Музыка выключена");
+        }
+
+        public void SetVolume(int volume)
+        {
+            Volume = volume;
+            Console.WriteLine($"Громкость: {Volume}");
+        }
+    }
+
+    class LightOnCommand : ICommand
+    {
+        private Light light;
+
+        public LightOnCommand(Light light)
+        {
+            this.light = light;
+        }
+
+        public void Execute() => light.On();
+
+        public void Undo() => light.Off();
+
+        public string GetName() => "Включить свет";
+    }
+
+    class LightOffCommand : ICommand
+    {
+        private Light light;
+
+        public LightOffCommand(Light light)
+        {
+            this.light = light;
+        }
+
+        public void Execute() => light.Off();
+
+        public void Undo() => light.On();
+
+        public string GetName() => "Выключить свет";
+    }
+
+    class ACOnCommand : ICommand
+    {
+        private AirConditioner ac;
+
+        public ACOnCommand(AirConditioner ac)
+        {
+            this.ac = ac;
+        }
+
+        public void Execute() => ac.On();
+
+        public void Undo() => ac.Off();
+
+        public string GetName() => "Включить кондиционер";
+    }
+
+    class ACOffCommand : ICommand
+    {
+        private AirConditioner ac;
+
+        public ACOffCommand(AirConditioner ac)
+        {
+            this.ac = ac;
+        }
+
+        public void Execute() => ac.Off();
+
+        public void Undo() => ac.On();
+
+        public string GetName() => "Выключить кондиционер";
+    }
+
+    class ACSetTempCommand : ICommand
+    {
+        private AirConditioner ac;
+        private int newTemp;
+        private int oldTemp;
+
+        public ACSetTempCommand(AirConditioner ac, int temp)
+        {
+            this.ac = ac;
+            newTemp = temp;
+        }
+
+        public void Execute()
+        {
+            oldTemp = ac.Temperature;
+            ac.SetTemperature(newTemp);
+        }
+
+        public void Undo()
+        {
+            ac.SetTemperature(oldTemp);
+        }
+
+        public string GetName()
+        {
+            return $"Установить температуру {newTemp}C";
+        }
+    }
+
+    class MusicOnCommand : ICommand
+    {
+        private MusicSystem music;
+
+        public MusicOnCommand(MusicSystem music)
+        {
+            this.music = music;
+        }
+
+        public void Execute() => music.On();
+
+        public void Undo() => music.Off();
+
+        public string GetName() => "Включить музыку";
+    }
+
+    class MusicOffCommand : ICommand
+    {
+        private MusicSystem music;
+
+        public MusicOffCommand(MusicSystem music)
+        {
+            this.music = music;
+        }
+
+        public void Execute() => music.Off();
+
+        public void Undo() => music.On();
+
+        public string GetName() => "Выключить музыку";
+    }
+
+    class MusicSetVolumeCommand : ICommand
+    {
+        private MusicSystem music;
+        private int newVolume;
+        private int oldVolume;
+
+        public MusicSetVolumeCommand(MusicSystem music, int volume)
+        {
+            this.music = music;
+            newVolume = volume;
+        }
+
+        public void Execute()
+        {
+            oldVolume = music.Volume;
+            music.SetVolume(newVolume);
+        }
+
+        public void Undo()
+        {
+            music.SetVolume(oldVolume);
+        }
+
+        public string GetName()
+        {
+            return $"Установить громкость {newVolume}";
+        }
+    }
+
+    class MacroCommand : ICommand
+    {
+        private List<ICommand> commands;
+
+        public MacroCommand(List<ICommand> commands)
+        {
+            this.commands = commands;
+        }
+
+        public void Execute()
+        {
+            foreach (var command in commands)
+            {
+                command.Execute();
+            }
+        }
+
+        public void Undo()
+        {
+            for (int i = commands.Count - 1; i >= 0; i--)
+            {
+                commands[i].Undo();
+            }
+        }
+
+        public string GetName()
+        {
+            return $"Макрос: {commands.Count} команд";
+        }
+    }
+
+    class RemoteControl
+    {
+        private ICommand[] commands = new ICommand[8];
+        private Stack<ICommand> history = new Stack<ICommand>();
+
+        public void SetCommand(int slot, ICommand command)
+        {
+            commands[slot] = command;
+        }
+
+        public void PressButton(int slot)
+        {
+            if (commands[slot] == null)
+            {
+                Console.WriteLine("Команда не назначена");
+                return;
+            }
+
+            Console.WriteLine($"Нажата кнопка {slot}: {commands[slot].GetName()}");
+
+            commands[slot].Execute();
+            history.Push(commands[slot]);
+        }
+
+        public void PressUndo()
+        {
+            if (history.Count == 0)
+            {
+                Console.WriteLine("История пуста");
+                return;
+            }
+
+            ICommand command = history.Pop();
+
+            Console.WriteLine($"Отмена: {command.GetName()}");
+
+            command.Undo();
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Light l = new Light();
+            AirConditioner ac = new AirConditioner();
+            MusicSystem m = new MusicSystem();
+
+            RemoteControl rc = new RemoteControl();
+
+            rc.SetCommand(0, new LightOnCommand(l));
+            rc.SetCommand(1, new LightOffCommand(l));
+            rc.SetCommand(2, new ACOnCommand(ac));
+            rc.SetCommand(3, new ACOffCommand(ac));
+            rc.SetCommand(4, new ACSetTempCommand(ac, 25));
+            rc.SetCommand(5, new MusicOnCommand(m));
+            rc.SetCommand(6, new MusicOffCommand(m));
+
+            rc.PressButton(0);
+            rc.PressButton(4);
+            rc.PressButton(6);
+
+            rc.PressUndo();
+            rc.PressUndo();
+            rc.PressUndo();
+
+            MacroCommand mc = new MacroCommand(
+                new List<ICommand>{
+                    new LightOnCommand(l),
+                    new ACSetTempCommand(ac, 24),
+                    new MusicOnCommand(m),
+                    new MusicSetVolumeCommand(m, 30)
+                }
+            );
+
+            rc.SetCommand(7, mc);
+
+            rc.PressButton(7);
+
+            rc.PressUndo();
+        }
+    }
+}
---
+++ PatternState/Program.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternState
+{
+    interface ICoffeeMachineState
+    {
+        void InsertMoney(decimal amount);
+        void SelectDrink(string drink);
+        void Cancel();
+        void Confirm();
+        void TakeDrink();
+        string GetStateName();
+    }
+
+    class CoffeeMachine
+    {
+        private ICoffeeMachineState currentState;
+
+        public decimal Money { get; set; }
+        public string SelectedDrink { get; set; }
+
+        public CoffeeMachine()
+        {
+            currentState = new WaitingForPaymentState(this);
+        }
+
+        public void SetState(ICoffeeMachineState state)
+        {
+            currentState = state;
+            Console.WriteLine($"Состояние: {currentState.GetStateName()}");
+        }
+
+        public void InsertMoney(decimal amount)
+        {
+            currentState.InsertMoney(amount);
+        }
+
+        public void SelectDrink(string drink)
+        {
+            currentState.SelectDrink(drink);
+        }
+
+        public void Cancel()
+        {
+            currentState.Cancel();
+        }
+
+        public void Confirm()
+        {
+            currentState.Confirm();
+        }
+
+        public void TakeDrink()
+        {
+            currentState.TakeDrink();
+        }
+    }
+
+    class WaitingForPaymentState : ICoffeeMachineState
+    {
+        private CoffeeMachine machine;
+
+        public WaitingForPaymentState(CoffeeMachine machine)
+        {
+            this.machine = machine;
+        }
+
+        public void InsertMoney(decimal amount)
+        {
+            machine.Money += amount;
+
+            Console.WriteLine($"Внесено {amount} руб.");
+
+            machine.SetState(new DrinkSelectionState(machine));
+        }
+
+        public void SelectDrink(string drink)
+        {
+            Console.WriteLine("Сначала внесите деньги");
+        }
+
+        public void Cancel()
+        {
+            Console.WriteLine("Отменять нечего");
+        }
+
+        public void Confirm()
+        {
+            Console.WriteLine("Сначала выберите напиток");
+        }
+
+        public void TakeDrink()
+        {
+            Console.WriteLine("Напиток не готов");
+        }
+
+        public string GetStateName()
+        {
+            return "Ожидание оплаты";
+        }
+    }
+
+    class DrinkSelectionState : ICoffeeMachineState
+    {
+        private CoffeeMachine machine;
+
+        public DrinkSelectionState(CoffeeMachine machine)
+        {
+            this.machine = machine;
+        }
+
+        public void InsertMoney(decimal amount)
+        {
+            Console.WriteLine("Деньги уже внесены");
+        }
+
+        public void SelectDrink(string drink)
+        {
+            machine.SelectedDrink = drink;
+
+            Console.WriteLine($"Выбран напиток: {drink}");
+        }
+
+        public void Cancel()
+        {
+            Console.WriteLine("Заказ отменен");
+
+            machine.Money = 0;
+            machine.SelectedDrink = "";
+
+            machine.SetState(new WaitingForPaymentState(machine));
+        }
+
+        public void Confirm()
+        {
+            if (string.IsNullOrEmpty(machine.SelectedDrink))
+            {
+                Console.WriteLine("Сначала выберите напиток");
+                return;
+            }
+
+            machine.SetState(new BrewingState(machine));
+        }
+
+        public void TakeDrink()
+        {
+            Console.WriteLine("Напиток еще не готов");
+        }
+
+        public string GetStateName()
+        {
+            return "Выбор напитка";
+        }
+    }
+
+    class BrewingState : ICoffeeMachineState
+    {
+        private CoffeeMachine machine;
+
+        public BrewingState(CoffeeMachine machine)
+        {
+            this.machine = machine;
+
+            Console.WriteLine($"Готовится {machine.SelectedDrink}...");
+
+            machine.SetState(new DispensingState(machine));
+        }
+
+        public void InsertMoney(decimal amount)
+        {
+            Console.WriteLine("Идет приготовление");
+        }
+
+        public void SelectDrink(string drink)
+        {
+            Console.WriteLine("Идет приготовление");
+        }
+
+        public void Cancel()
+        {
+            Console.WriteLine("Отменить нельзя");
+        }
+
+        public void Confirm()
+        {
+            Console.WriteLine("Напиток уже готовится");
+        }
+
+        public void TakeDrink()
+        {
+            Console.WriteLine("Подождите приготовления");
+        }
+
+        public string GetStateName()
+        {
+            return "Приготовление";
+        }
+    }
+
+    class DispensingState : ICoffeeMachineState
+    {
+        private CoffeeMachine machine;
+
+        public DispensingState(CoffeeMachine machine)
+        {
+            this.machine = machine;
+        }
+
+        public void InsertMoney(decimal amount)
+        {
+            Console.WriteLine("Сначала заберите напиток");
+        }
+
+        public void SelectDrink(string drink)
+        {
+            Console.WriteLine("Сначала заберите напиток");
+        }
+
+        public void Cancel()
+        {
+            Console.WriteLine("Отменить нельзя");
+        }
+
+        public void Confirm()
+        {
+            Console.WriteLine("Напиток уже готов");
+        }
+
+        public void TakeDrink()
+        {
+            Console.WriteLine($"Вы получили: {machine.SelectedDrink}");
+
+            machine.Money = 0;
+            machine.SelectedDrink = "";
+
+            machine.SetState(new WaitingForPaymentState(machine));
+        }
+
+        public string GetStateName()
+        {
+            return "Выдача напитка";
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CoffeeMachine cm = new CoffeeMachine();
+            cm.InsertMoney(150);
+            cm.SelectDrink("Капучино");
+            cm.Confirm();
+            cm.TakeDrink();
+
+        }
+    }
+}
